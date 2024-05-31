f40ee790a6a892e439a375bf050cbbc7cccd1429
Matthew Josephson <167241186+mjosephson7@users.noreply.github.com>
2024-05-31 03:00:54 -0400

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+/**
+ *
+ * @author Matthew Josephson
+ */
+
+public class Main {
+    public static void main(String[] arg) {
+        LoginPage login = new LoginPage();
+        login.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
